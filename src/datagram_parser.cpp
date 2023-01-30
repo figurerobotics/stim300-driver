8f6bb2e816d18a6f214d9b1d6f0d852d2bcdadbe
Mark Johnston <mark@ologicinc.com>
2023-01-29 16:27:49 -0800

    Adding the saving within SensorConfig many of the items from reading the configuration from the device. It is later to be possible to add methods to read these back to upper layers.  A lot of formating and cosmetic changes were done to better match our own code standards

--- src/datagram_parser.cpp
+++ src/datagram_parser.cpp
@@ -1,14 +1,15 @@
+// This file is from a stim300 driver package available under MIT license
+// File comes from https://github.com/vortexntnu/stim300-driver
+// Copyright (c) 2019 Vortex NTNU  MIT License
 
-#include "datagram_parser.h"
+#include "apps/imu_app/datagram_parser.h"
+#include <spdlog/spdlog.h>
 
-using namespace stim_const;
-using namespace stim_300;
+namespace stim_300 {
 
-DatagramParser::DatagramParser(DatagramIdentifier dg_id, GyroOutputUnit gyro_o,
-                               AccOutputUnit acc_o, InclOutputUnit incl_o,
-                               AccRange acc_range)
-    : is_included_(isIncluded(dg_id)), temp_scale_(tempScale()),
-      aux_scale_(auxScale()) {
+DatagramParser::DatagramParser(DatagramIdentifier dg_id, GyroOutputUnit gyro_o, AccOutputUnit acc_o,
+                               InclOutputUnit incl_o, AccRange acc_range)
+    : is_included_(isIncluded(dg_id)), temp_scale_(tempScale()), aux_scale_(auxScale()) {
   setDataScales(gyro_o, acc_o, incl_o, acc_range);
 }
 void DatagramParser::setDataParameters(SensorConfig sensor_config) {
@@ -19,34 +20,41 @@ void DatagramParser::setDataParameters(SensorConfig sensor_config) {
 void DatagramParser::setDataScales(GyroOutputUnit gyro_o, AccOutputUnit acc_o,
                                    InclOutputUnit incl_o, AccRange acc_range) {
   switch (gyro_o) {
-  case GyroOutputUnit::ANGULAR_RATE:         // units are in rad/s
-  case GyroOutputUnit::AVERAGE_ANGULAR_RATE: // units are in rad/s
-    gyro_scale_ = gyroScale();
-    break;
-  case GyroOutputUnit::INCREMENTAL_ANGLE: // units are in rad/sample
-  case GyroOutputUnit::INTEGRATED_ANGLE:  // units are in rad
-    gyro_scale_ = gyroIncrScale();
-    break;
+      // for units in rad/s
+    case GyroOutputUnit::ANGULAR_RATE:
+      // for units in rad/s
+    case GyroOutputUnit::AVERAGE_ANGULAR_RATE:
+      gyro_scale_ = gyroScale();
+      break;
+      // for units in rad/sample
+    case GyroOutputUnit::INCREMENTAL_ANGLE:
+      // for units in rad
+    case GyroOutputUnit::INTEGRATED_ANGLE:
+      gyro_scale_ = gyroIncrScale();
+      break;
   }
   switch (acc_o) {
-  case AccOutputUnit::ACCELERATION:         // units are in g
-  case AccOutputUnit::AVERAGE_ACCELERATION: // units are in g
-    acc_scale_ = accScale(acc_range);
-    break;
-  case AccOutputUnit::INCREMENTAL_VELOCITY: // units are in m/s/sample
-  case AccOutputUnit::INTEGRATED_VELOCITY:
-    acc_scale_ = accIncrScale(acc_range);
-    break;
+      // for units in g
+    case AccOutputUnit::ACCELERATION:
+      // for units in g
+    case AccOutputUnit::AVERAGE_ACCELERATION:
+      acc_scale_ = accScale(acc_range);
+      break;
+      // for units in m/s/sample
+    case AccOutputUnit::INCREMENTAL_VELOCITY:
+    case AccOutputUnit::INTEGRATED_VELOCITY:
+      acc_scale_ = accIncrScale(acc_range);
+      break;
   }
   switch (incl_o) {
-  case InclOutputUnit::ACCELERATION:         // units are in g
-  case InclOutputUnit::AVERAGE_ACCELERATION: // units are in g
-    incl_scale_ = inclScale();
-    break;
-  case InclOutputUnit::INCREMENTAL_VELOCITY: // units are in m/s/sample
-  case InclOutputUnit::INTEGRATED_VELOCITY:
-    incl_scale_ = inclIncrScale();
-    break;
+    case InclOutputUnit::ACCELERATION:          // units are in g
+    case InclOutputUnit::AVERAGE_ACCELERATION:  // units are in g
+      incl_scale_ = inclScale();
+      break;
+    case InclOutputUnit::INCREMENTAL_VELOCITY:  // units are in m/s/sample
+    case InclOutputUnit::INTEGRATED_VELOCITY:
+      incl_scale_ = inclIncrScale();
+      break;
   }
 }
 
@@ -54,52 +62,51 @@ uint32_t DatagramParser::parseCRC(std::vector<uint8_t>::const_iterator &&itr) {
   return parseUnsigned(itr, N_BYTES_CRC);
 }
 
-uint8_t
-DatagramParser::parseData(std::vector<uint8_t>::const_iterator &&buffer_itr,
-                          SensorData &sensor_data) const {
+// Given the raw Normal Mode datagram described on STIM300 datasheet
+// section 5.5.6 Parse out X,Y,Z and status for Gyro, Accel, Incline, gyroTemp,
+// accelTemp, inclinometer temp After that pull in the rest as may be required.
+uint8_t DatagramParser::parseData(std::vector<uint8_t>::const_iterator &&buffer_itr,
+                                  SensorData &sensor_data) const {
   uint8_t status{0};
-  buffer_itr++; // Skip datagram identifier
+  buffer_itr++;  // Skip datagram identifier
   if (is_included_[SensorIndx::GYRO]) {
     for (auto &gyro : sensor_data.gyro)
-      gyro = gyro_scale_ *
-             parseTwosComplement(buffer_itr, N_BYTES_INERTIAL_SENSOR);
+      gyro = gyro_scale_ * parseTwosComplement(buffer_itr, N_BYTES_INERTIAL_SENSOR);
     status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
   }
   if (is_included_[SensorIndx::ACC]) {
     for (auto &acc : sensor_data.acc)
-      acc =
-          acc_scale_ * parseTwosComplement(buffer_itr, N_BYTES_INERTIAL_SENSOR);
+      acc = acc_scale_ * parseTwosComplement(buffer_itr, N_BYTES_INERTIAL_SENSOR);
     status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
   }
   if (is_included_[SensorIndx::INCL]) {
     for (auto &incl : sensor_data.incl)
-      incl = incl_scale_ *
-             parseTwosComplement(buffer_itr, N_BYTES_INERTIAL_SENSOR);
+      incl = incl_scale_ * parseTwosComplement(buffer_itr, N_BYTES_INERTIAL_SENSOR);
     status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
   }
   if (is_included_[SensorIndx::TEMP]) {
     if (is_included_[SensorIndx::GYRO]) {
-      for (auto &temp : sensor_data.temp_gyro)
-        temp =
-            temp_scale_ * parseTwosComplement(buffer_itr, N_BYTES_TEMP_SENSOR);
+      for (auto &temp : sensor_data.temp_gyro) {
+        int32_t tempVal = parseTwosComplement(buffer_itr, N_BYTES_TEMP_SENSOR);
+        temp = tempScale() * tempVal;
+      }
       status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
     }
     if (is_included_[SensorIndx::ACC]) {
-      for (auto &temp : sensor_data.temp_acc)
-        temp =
-            temp_scale_ * parseTwosComplement(buffer_itr, N_BYTES_TEMP_SENSOR);
+      for (auto &temp : sensor_data.temp_acc) {
+        int32_t tempVal = parseTwosComplement(buffer_itr, N_BYTES_TEMP_SENSOR);
+        temp = tempScale() * tempVal;
+      }
       status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
     }
     if (is_included_[SensorIndx::INCL]) {
       for (auto &temp : sensor_data.temp_incl)
-        temp =
-            tempScale() * parseTwosComplement(buffer_itr, N_BYTES_TEMP_SENSOR);
+        temp = tempScale() * parseTwosComplement(buffer_itr, N_BYTES_TEMP_SENSOR);
       status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
     }
   }
   if (is_included_[SensorIndx::AUX]) {
-    sensor_data.aux =
-        aux_scale_ * parseTwosComplement(buffer_itr, N_BYTES_AUX_SENSOR);
+    sensor_data.aux = aux_scale_ * parseTwosComplement(buffer_itr, N_BYTES_AUX_SENSOR);
     status |= parseUnsigned(buffer_itr, N_BYTES_STATUS);
   }
   sensor_data.counter = parseUnsigned(buffer_itr, N_BYTES_COUNTER);
@@ -109,31 +116,40 @@ DatagramParser::parseData(std::vector<uint8_t>::const_iterator &&buffer_itr,
   return status;
 }
 
-SensorConfig DatagramParser::parseConfig(
-    std::vector<uint8_t>::const_iterator &&buffer_itr) const {
+SensorConfig DatagramParser::parseConfig(std::vector<uint8_t>::const_iterator &&buffer_itr) const {
   SensorConfig sensor_config{};
   sensor_config.revision = buffer_itr[1];
-  sensor_config.firmvare_version = buffer_itr[2];
+  sensor_config.firmware_version = buffer_itr[2];
   switch (buffer_itr[3] >> 5u) {
-  case 0u:
-    sensor_config.sample_freq = SampleFreq::S125;
-    break;
-  case 1u:
-    sensor_config.sample_freq = SampleFreq::S250;
-    break;
-  case 2u:
-    sensor_config.sample_freq = SampleFreq::S500;
-    break;
-  case 3u:
-    sensor_config.sample_freq = SampleFreq::S1000;
-    break;
-  case 4u:
-    sensor_config.sample_freq = SampleFreq::S2000;
-    break;
-  case 5u:
-    sensor_config.sample_freq = SampleFreq::TRG;
-    break;
+    case 0u:
+      sensor_config.sample_freq = SampleFreq::S125;
+      break;
+    case 1u:
+      sensor_config.sample_freq = SampleFreq::S250;
+      break;
+    case 2u:
+      sensor_config.sample_freq = SampleFreq::S500;
+      break;
+    case 3u:
+      sensor_config.sample_freq = SampleFreq::S1000;
+      break;
+    case 4u:
+      sensor_config.sample_freq = SampleFreq::S2000;
+      break;
+    case 5u:
+      sensor_config.sample_freq = SampleFreq::TRG;
+      break;
   }
+  sensor_config.sys_config_1 = buffer_itr[3];
+  sensor_config.sys_config_2 = buffer_itr[4];
+
+  // Accel and gyro filters are packed nibbles with enable states and filter
+  // settings Upper nibble is XYZ enables.  Then next nibbles are X, Y, Z filter
+  // settings
+  sensor_config.gyro_filters = ((buffer_itr[5] & 0x70) << 8) | ((buffer_itr[6] & 0x77) << 4) |
+                               ((buffer_itr[7] & 0x70) >> 4);
+  sensor_config.accel_filters = ((buffer_itr[8] & 0x70) << 8) | ((buffer_itr[9] & 0x77) << 4) |
+                                ((buffer_itr[10] & 0x70) >> 4);
 
   sensor_config.normal_datagram_CRLF = (buffer_itr[3] & (1 << 0));
   std::array<bool, 5> included_sensors{};
@@ -145,78 +161,79 @@ SensorConfig DatagramParser::parseConfig(
   sensor_config.datagram_id = toDatagramID(included_sensors);
 
   switch (buffer_itr[5] & 0b00001111) {
-  case 0:
-    sensor_config.gyro_output_unit = GyroOutputUnit::ANGULAR_RATE;
-    break;
-  case 1:
-    sensor_config.gyro_output_unit = GyroOutputUnit::INCREMENTAL_ANGLE;
-    break;
-  case 2:
-    sensor_config.gyro_output_unit = GyroOutputUnit::AVERAGE_ANGULAR_RATE;
-    break;
-  case 3:
-    sensor_config.gyro_output_unit = GyroOutputUnit::INTEGRATED_ANGLE;
-    break;
-  case 4:
-    sensor_config.gyro_output_unit = GyroOutputUnit::ANGULAR_RATE;
-    break;
-  case 5:
-    sensor_config.gyro_output_unit = GyroOutputUnit::INCREMENTAL_ANGLE;
-    break;
-  case 6:
-    sensor_config.gyro_output_unit = GyroOutputUnit::AVERAGE_ANGULAR_RATE;
-    break;
-  case 7:
-    sensor_config.gyro_output_unit = GyroOutputUnit::INTEGRATED_ANGLE;
-    break;
+    case 0:
+      sensor_config.gyro_output_unit = GyroOutputUnit::ANGULAR_RATE;
+      break;
+    case 1:
+      sensor_config.gyro_output_unit = GyroOutputUnit::INCREMENTAL_ANGLE;
+      break;
+    case 2:
+      sensor_config.gyro_output_unit = GyroOutputUnit::AVERAGE_ANGULAR_RATE;
+      break;
+    case 3:
+      sensor_config.gyro_output_unit = GyroOutputUnit::INTEGRATED_ANGLE;
+      break;
+    case 4:
+      sensor_config.gyro_output_unit = GyroOutputUnit::ANGULAR_RATE;
+      break;
+    case 5:
+      sensor_config.gyro_output_unit = GyroOutputUnit::INCREMENTAL_ANGLE;
+      break;
+    case 6:
+      sensor_config.gyro_output_unit = GyroOutputUnit::AVERAGE_ANGULAR_RATE;
+      break;
+    case 7:
+      sensor_config.gyro_output_unit = GyroOutputUnit::INTEGRATED_ANGLE;
+      break;
   }
 
   switch (buffer_itr[8] & 0b00001111) {
-  case 0:
-    sensor_config.acc_output_unit = AccOutputUnit::ACCELERATION;
-    break;
-  case 1:
-    sensor_config.acc_output_unit = AccOutputUnit::INCREMENTAL_VELOCITY;
-    break;
-  case 2:
-    sensor_config.acc_output_unit = AccOutputUnit::AVERAGE_ACCELERATION;
-    break;
-  case 3:
-    sensor_config.acc_output_unit = AccOutputUnit::INTEGRATED_VELOCITY;
-    break;
+    case 0:
+      sensor_config.acc_output_unit = AccOutputUnit::ACCELERATION;
+      break;
+    case 1:
+      sensor_config.acc_output_unit = AccOutputUnit::INCREMENTAL_VELOCITY;
+      break;
+    case 2:
+      sensor_config.acc_output_unit = AccOutputUnit::AVERAGE_ACCELERATION;
+      break;
+    case 3:
+      sensor_config.acc_output_unit = AccOutputUnit::INTEGRATED_VELOCITY;
+      break;
   }
 
   switch (buffer_itr[11] & 0b00001111) {
-  case 0:
-    sensor_config.incl_output_unit = InclOutputUnit::ACCELERATION;
-    break;
-  case 1:
-    sensor_config.incl_output_unit = InclOutputUnit::INCREMENTAL_VELOCITY;
-    break;
-  case 2:
-    sensor_config.incl_output_unit = InclOutputUnit::AVERAGE_ACCELERATION;
-    break;
-  case 3:
-    sensor_config.incl_output_unit = InclOutputUnit::INTEGRATED_VELOCITY;
-    break;
+    case 0:
+      sensor_config.incl_output_unit = InclOutputUnit::ACCELERATION;
+      break;
+    case 1:
+      sensor_config.incl_output_unit = InclOutputUnit::INCREMENTAL_VELOCITY;
+      break;
+    case 2:
+      sensor_config.incl_output_unit = InclOutputUnit::AVERAGE_ACCELERATION;
+      break;
+    case 3:
+      sensor_config.incl_output_unit = InclOutputUnit::INTEGRATED_VELOCITY;
+      break;
   }
   // Assuming all the axes has the same range
   switch (buffer_itr[17] >> 4) {
-  case 0:
-    sensor_config.acc_range = AccRange::G10;
-    break;
-  case 2:
-    sensor_config.acc_range = AccRange::G2;
-    break;
-  case 3:
-    sensor_config.acc_range = AccRange::G5;
-    break;
-  case 4:
-    sensor_config.acc_range = AccRange::G30;
-    break;
-  case 6:
-    sensor_config.acc_range = AccRange::G80;
-    break;
+    case 0:
+      sensor_config.acc_range = AccRange::G10;
+      break;
+    case 2:
+      sensor_config.acc_range = AccRange::G2;
+      break;
+    case 3:
+      sensor_config.acc_range = AccRange::G5;
+      break;
+    case 4:
+      sensor_config.acc_range = AccRange::G30;
+      break;
+    case 6:
+      sensor_config.acc_range = AccRange::G80;
+      break;
   }
   return sensor_config;
 }
+}  // namespace stim_300
--- src/datagram_parser.h
+++ src/datagram_parser.h
@@ -1,19 +1,25 @@
+// This file is from a stim300 driver package available under MIT license
+// File comes from https://github.com/vortexntnu/stim300-driver
+// Copyright (c) 2019 Vortex NTNU  MIT License
 
-#ifndef DRIVER_STIM300_DATAGRAM_PARSER_H
-#define DRIVER_STIM300_DATAGRAM_PARSER_H
+#pragma once
 
-#include "stim300_constants.h"
 #include <array>
 #include <cassert>
 #include <cstdint>
 #include <sstream>
 #include <vector>
-using namespace stim_const;
+
+#include "apps/imu_app/stim300_constants.h"
 
 namespace stim_300 {
 struct SensorConfig {
   char revision;
-  uint8_t firmvare_version;
+  uint8_t firmware_version;
+  uint8_t sys_config_1;
+  uint8_t sys_config_2;
+  uint16_t accel_filters;
+  uint16_t gyro_filters;
   SampleFreq sample_freq;
   DatagramIdentifier datagram_id;
   bool normal_datagram_CRLF;
@@ -23,13 +29,13 @@ struct SensorConfig {
   AccRange acc_range;
 
   inline bool operator!=(const SensorConfig &rhs) {
-    return this->sample_freq != rhs.sample_freq or
-           this->datagram_id != rhs.datagram_id or
-           this->normal_datagram_CRLF != rhs.normal_datagram_CRLF or
-           this->gyro_output_unit != rhs.gyro_output_unit or
-           this->acc_output_unit != rhs.acc_output_unit or
-           this->incl_output_unit != rhs.incl_output_unit or
-           this->acc_range != rhs.acc_range;
+    bool val = false;
+    val = (this->sample_freq != rhs.sample_freq || this->datagram_id != rhs.datagram_id ||
+           this->normal_datagram_CRLF != rhs.normal_datagram_CRLF ||
+           this->gyro_output_unit != rhs.gyro_output_unit ||
+           this->acc_output_unit != rhs.acc_output_unit ||
+           this->incl_output_unit != rhs.incl_output_unit || this->acc_range != rhs.acc_range);
+    return val;
   }
 };
 
@@ -46,30 +52,31 @@ struct SensorData {
 };
 
 struct DatagramParser {
-  DatagramParser(DatagramIdentifier dg_id, GyroOutputUnit gyro_o,
-                 AccOutputUnit acc_o, InclOutputUnit incl_o,
-                 AccRange acc_range);
+  DatagramParser(DatagramIdentifier dg_id, GyroOutputUnit gyro_o, AccOutputUnit acc_o,
+                 InclOutputUnit incl_o, AccRange acc_range);
   void setDataParameters(SensorConfig sensor_config);
   static uint32_t parseCRC(std::vector<uint8_t>::const_iterator &&itr);
+
+  /// @brief Parse a complete datagram into data values held in the parser object
   uint8_t parseData(std::vector<uint8_t>::const_iterator &&buffer_itr,
                     SensorData &sensor_data) const;
-  SensorConfig
-  parseConfig(std::vector<uint8_t>::const_iterator &&buffer_itr) const;
 
-private:
+  /// @brief Parse a complete onfiguration datagram into config state held in the parser object
+  SensorConfig parseConfig(std::vector<uint8_t>::const_iterator &&buffer_itr) const;
+
+ private:
   std::array<bool, 5> is_included_;
   double temp_scale_;
   double aux_scale_;
   double gyro_scale_;
   double acc_scale_;
   double incl_scale_;
-  void setDataScales(GyroOutputUnit gyro_o, AccOutputUnit acc_o,
-                     InclOutputUnit incl_o, AccRange acc_range);
+  void setDataScales(GyroOutputUnit gyro_o, AccOutputUnit acc_o, InclOutputUnit incl_o,
+                     AccRange acc_range);
   // Meta data is stored as "unsigned word", we simply combine the bytes into
   // the right sized uint by left shifting them. Note the biggest is the CRC
   // which is 32 bits.
-  static const uint32_t parseUnsigned(std::vector<uint8_t>::const_iterator &it,
-                                      uint8_t size) {
+  static uint32_t parseUnsigned(std::vector<uint8_t>::const_iterator &it, uint8_t size) {
     uint32_t tmp{0};
     for (int i = 0; i < size; ++i) {
       tmp = (tmp << 8u) | *(it++);
@@ -81,17 +88,15 @@ private:
   // the datasheet, bu we shift them to fill up the int32_t type then we shift
   // them back to their right position. When we shift them back the shift
   // operator will automatically sign-extend the value.
-  static const int32_t
-  parseTwosComplement(std::vector<uint8_t>::const_iterator &it,
-                      const uint8_t size) {
-    assert(size == 3 or size == 2);
+  static int32_t parseTwosComplement(std::vector<uint8_t>::const_iterator &it, const uint8_t size) {
+    assert(size == 3 || size == 2);
     if (size == 3) {
       return ((*it++ << 24) | (*it++ << 16) | (*it++ << 8)) >> 8;
     } else if (size == 2) {
       return ((*it++ << 24) | (*it++ << 16)) >> 16;
     }
+    return 0;
   }
 };
-} // end namespace stim_300
-
-#endif // DRIVER_STIM300_DATAGRAM_PARSER_H
+// end namespace stim_300
+}  // namespace stim_300
